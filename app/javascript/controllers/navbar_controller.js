c075956a05f35c80c893d03ac064362941cd2057
Pierre Cailliau <cailliau.pierre@gmail.com>
2022-08-23 14:55:40 +0200

    done

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -4,15 +4,18 @@ export default class extends Controller {
   static targets = ["navbar", "banner"]
 
   connect() {
+    console.log('hello')
     if (this.hasBannerTarget) {
-      this.navbarTarget.style.backgroundColor = "transparent"
+      this.navbarTarget.classList.add("navbar-lewagon-transparent")
     }
   }
 
   updateNavbar() {
+    console.log("hi")
     if (!this.hasBannerTarget) {
       return
     }
+    console.log({offset: this.bannerTarget.offsetHeight / 2, scrollY: window.scrollY})
     if (window.scrollY >= (this.bannerTarget.offsetHeight / 2)) {
       this.navbarTarget.classList.add("navbar-lewagon-black")
     } else {
