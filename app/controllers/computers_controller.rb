7cb106c6aabbc14d18dde83e8e07f538d09a53e9
Cristina Nunu <nunucristina0@gmail.com>
2022-08-23 14:03:29 +0200

    Adding computer index and show view

--- app/controllers/computers_controller.rb
+++ app/controllers/computers_controller.rb
@@ -1,7 +1,12 @@
 class ComputersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
 
   def index
     @computers = Computer.all
+  end
+
+  def show
+    @computer = Computer.find(params[:id])
   end
 
 end
